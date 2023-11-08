629daf6c867409e04011aaa1fab47158dc4fc52d
Dee <deepakdilse@gmail.com>
2023-11-08 12:34:15 -0800

    add login/signup functionality

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,9 +1,21 @@
 const router = require('express').Router();
 
+const { User } = require('../models');
 const {withAuth, areAuth } = require('../utils/auth');
 
-router.get('/',(req,res)=> {
-  res.render('homepage');
+router.get('/', async (req,res)=> {
+  try {
+    console.log('userid in sess :', req.session.userId, req.session.loggedIn);
+    // const dbUserData = await User.findByPk(req.session.userId);
+    // const userData = dbUserData.get({ plain: true });
+    res.render('homepage', {
+      loggedIn: req.session.loggedIn,
+    });
+  } catch(err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+
 })
 
 router.get('/login', areAuth, (req, res) => {
